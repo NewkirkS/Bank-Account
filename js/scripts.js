ca08e19662f25183d0bf2e503c17503072119ee7
chris martinez and Stephen Newkirk <chris@>
2016-08-23 14:11:06 -0700

    Program will add or subtract from account balance based on user input.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,12 @@ function Account (name, deposit) {
   this.accountName = name;
   this.accountBalance = deposit;
 }
+Account.prototype.withdraw = function(withdrawl){
+  this.accountBalance = this.accountBalance - withdrawl
+}
+Account.prototype.deposit = function(deposit){
+  this.accountBalance = this.accountBalance + deposit
+}
 //Front End
 $(function(){
   $("form#depositForm").submit(function(event){
@@ -10,7 +16,16 @@ $(function(){
   var accountName =  $("input#user-name").val();
   var initialDeposit =  parseFloat($("input#initial-deposit").val());
   var newAccount = new Account(accountName, initialDeposit);
-  console.log(newAccount);
+  $("#withdrawButton").click(function(event){
+    var withdrawl = parseFloat($("#withdrawl").val());
+    newAccount.withdraw(withdrawl);
+    $("#results").text("$"+newAccount.accountBalance);
+  });
+  $("#depositButton").click(function(event){
+    var deposit = parseFloat($("#deposit").val());
+    newAccount.deposit(deposit);
+    $("#results").text("$"+newAccount.accountBalance);
+  });
   $("#results").text("$"+newAccount.accountBalance);
   });
 });
