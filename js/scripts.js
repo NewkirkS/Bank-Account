64ed91bc657211d664cd3f91e2a44e290ba3ac40
chris martinez and Stephen Newkirk <stephen@>
2016-08-23 16:52:15 -0700

    will change background and add shake effect when user overdraws their account

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,32 @@ function Account (name, deposit) {
   this.accountBalance = deposit;
 }
 Account.prototype.withdraw = function(withdrawl){
-  this.accountBalance = this.accountBalance - withdrawl;
-  if(this.accountBalance < 0){
-    this.accountBalance = this.accountBalance -(withdrawl + 4000);
+  if(withdrawl > 0){
+    this.accountBalance = this.accountBalance - withdrawl;
+    if(this.accountBalance < 0){
+      this.accountBalance = this.accountBalance -(withdrawl + 4000);
+    }
+      this.accountBalance = Math.ceil(this.accountBalance * 100) / 100;
+    }
+  else{
+    alert("bad")
   }
-    this.accountBalance = Math.ceil(this.accountBalance * 100) / 100;
-}
+}//end withdraw prototype
+
 Account.prototype.deposit = function(deposit){
-  if(this.accountBalance < 0){
-    this.accountBalance = this.accountBalance + deposit/20;
+  if(deposit > 0){
+    if(this.accountBalance < 0){
+      this.accountBalance = this.accountBalance + deposit/20;
+    }
+    else{
+      this.accountBalance = this.accountBalance + deposit;
+    }
   }
   else{
-    this.accountBalance = this.accountBalance + deposit;
+    alert("bad")
   }
   this.accountBalance = Math.ceil(this.accountBalance * 100) / 100;
-}
+}//end deposit prototype
 //Front End
 $(function(){
   $("form#depositForm").submit(function(event){
@@ -34,16 +45,34 @@ $(function(){
     $("#withdrawButton").click(function(){
       var withdrawl = parseFloat($("#withdrawl").val());
       newAccount.withdraw(withdrawl);
+      if(newAccount.accountBalance < 0){
+        $("form").addClass("shake-hard");
+        $("h1").addClass("shake-hard");
+        $("#results").addClass("red");
+        $("h1").addClass("red");
+        $("h1").text("WELCOME TO HELLS FARGO");
+        $("body").addClass("hell");
+      }
       $("#results").text("$" + newAccount.accountBalance);
       $("input").val("");
     });
     $("#depositButton").click(function(){
+
       var deposit = parseFloat($("#deposit").val());
       newAccount.deposit(deposit);
+      if(newAccount.accountBalance > 0){
+        $("form").removeClass("shake-hard");
+        $("h1").removeClass("shake-hard");
+        $("#results").removeClass("red");
+        $("h1").removeClass("red");
+        $("h1").text("Welcome to Wells Fargo");
+        $("body").removeClass("hell");
+      }
       $("#results").text("$" + newAccount.accountBalance);
       $("input").val("");
     });
   });
+
 
   $("#results").text("$" + newAccount.accountBalance);
   $("input").val("");
